d269c52ba5c2844676eb5ca588121737287327e6
andrew <andrewafk2000@gmail.com>
2018-10-25 12:04:19 +0300

    Changed links in home.blade.php to relative.

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,7 +3,7 @@
     <head>
     <title>{{ trans('app.app_name') }}</title>
         {{ Html::style("css/bootstrap.min.css") }}
-        {{ Html::style('css/main.css?v=1.6') }}
+        {{ Html::style('css/main.css') }}
     </head>
     <body>
         <div class="container">
